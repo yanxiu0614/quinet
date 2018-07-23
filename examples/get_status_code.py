9c33b3fca1ffc15b8767681c2f576926905ac2fe
yanxiu0614 <654494146@qq.com>
2018-07-23 22:52:53 +0800

    Update get_status_code.py

--- examples/get_status_code.py
+++ examples/get_status_code.py
@@ -3,19 +3,16 @@ import requests
 
 url_list = ['http://qq.com/', 'http://baidu.com', 'http://taobao.com']
 
-
 def get_status_code(url):
     try:
-        res = requests.get(url)
+        res=requests.get(url)
+        status_code=res.status_code
     except Exception as e:
-        pass
-        res = 0
-    return res.status_code
+        status_code=0
+    return(status_code)
 
-
-def print_status_code(code):
-    print(code)
-
+def print_status_code(status_code):
+    print(status_code)
 
 client = Quinet(method=get_status_code, callback=print_status_code)
 
@@ -26,16 +23,20 @@ client.run()
 
 
 """
+from quinet.quinet import Quinet
+import requests
+
+url_list = ['http://qq.com/', 'http://baidu.com', 'http://taobao.com']
 
 def get_status_code(url):
     try:
         res=requests.get(url)
+        status_code=res.status_code
     except Exception as e:
-        pass
-        res=0
-    print(res.status_code)
+        status_code=0
+    print(status_code)
 
-client=Quient(method=get_status_code)
+client=Quinet(method=get_status_code)
 
 for url in url_list:
     client.waiting_queue.put(url)
